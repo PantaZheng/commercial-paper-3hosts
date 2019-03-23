a0d2a1636cc5034567bb5f70e6d22c65ead83d35
panta <pantazheng@vip.qq.com>
2019-03-23 16:46:38 +0800

    文件整理

--- organization/basic-network/vm1/vm1-start.sh
+++ organization/basic-network/vm1/vm1-start.sh
@@ -5,15 +5,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Exit on first error, print all commands.
+cd ..
 set -ev
 
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 export IMAGE_TAG="latest"
-docker-compose -f vm3-docker-compose.yml down
+docker-compose -f vm1-docker-compose.yml down
 
 
-docker-compose -f vm3-docker-compose.yml up -d
+docker-compose -f vm1-docker-compose.yml up -d
 
 # wait for Hyperledger Fabric to start
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
@@ -21,5 +22,4 @@ export FABRIC_START_TIMEOUT=3
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
-docker exec peer0.org2.example.com peer channel fetch newest mychannel.block -o orderer.example.com:7050 -c mychannel
-docker exec peer0.org2.example.com peer channel join -b mychannel.block
+
--- organization/basic-network/vm2/vm2-start.sh
+++ organization/basic-network/vm2/vm2-start.sh
@@ -5,15 +5,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Exit on first error, print all commands.
+cd ..
 set -ev
 
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 export IMAGE_TAG="latest"
-docker-compose -f vm1-docker-compose.yml down
+docker-compose -f vm2-docker-compose.yml down
 
 
-docker-compose -f vm1-docker-compose.yml up -d
+docker-compose -f vm2-docker-compose.yml up -d
 
 # wait for Hyperledger Fabric to start
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
@@ -21,4 +22,6 @@ export FABRIC_START_TIMEOUT=3
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
+docker exec peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 
+docker exec peer0.org1.example.com  peer channel join -b mychannel.block
