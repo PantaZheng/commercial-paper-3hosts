47c535b4dc03a41165f334911d7b401e6b53c254
panta <pantazheng@vip.qq.com>
2019-03-25 13:58:03 +0800

    vm2 install 环境添加

--- organization/basic-network/vm2-install.sh
+++ organization/basic-network/vm2-install.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+set -ev
+
 docker exec cliMagnetoCorp peer chaincode install -n papercontract -v 0 -p /opt/gopath/src/github.com/contract -l node
 docker exec cliMagnetoCorp peer chaincode instantiate -n papercontract -v 0 -l node -c '{"Args":["org.papernet.commercialpaper:instantiate"]}' -C mychannel -P "OR ('Org1MSP.member','Org2MSP.member')"`
