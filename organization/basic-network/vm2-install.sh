ef900dcd44c59736e47ddb87caf110d02a5e98b2
panta <pantazheng@vip.qq.com>
2019-03-25 14:08:44 +0800

    多余符号

--- organization/basic-network/vm2-install.sh
+++ organization/basic-network/vm2-install.sh
@@ -3,4 +3,4 @@
 set -ev
 
 docker exec cliMagnetoCorp peer chaincode install -n papercontract -v 0 -p /opt/gopath/src/github.com/contract -l node
-docker exec cliMagnetoCorp peer chaincode instantiate -n papercontract -v 0 -l node -c '{"Args":["org.papernet.commercialpaper:instantiate"]}' -C mychannel -P "OR ('Org1MSP.member','Org2MSP.member')"`
+docker exec cliMagnetoCorp peer chaincode instantiate -n papercontract -v 0 -l node -c '{"Args":["org.papernet.commercialpaper:instantiate"]}' -C mychannel -P "OR ('Org1MSP.member','Org2MSP.member')"
