be274e841e8588ee438a630df7475cbf15a04b83
panta <pantazheng@vip.qq.com>
2019-03-26 11:19:42 +0800

    vm3-addTowallet

--- organization/digibank/application/addToWallet.js
+++ organization/digibank/application/addToWallet.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const { FileSystemWallet, X509WalletMixin } = require('fabric-network');
 const path = require('path');
 
-const fixtures = path.resolve(__dirname, '../../../../basic-network');
+const fixtures = path.resolve(__dirname, '../../basic-network');
 
 // A wallet stores a collection of identities
 const wallet = new FileSystemWallet('../identity/user/balaji/wallet');
@@ -20,13 +20,13 @@ async function main() {
     try {
 
         // Identity to credentials to be stored in the wallet
-        const credPath = path.join(fixtures, '/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com');
-        const cert = fs.readFileSync(path.join(credPath, '/msp/signcerts/Admin@org1.example.com-cert.pem')).toString();
+        const credPath = path.join(fixtures, '/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com');
+        const cert = fs.readFileSync(path.join(credPath, '/msp/signcerts/Admin@org2.example.com-cert.pem')).toString();
         const key = fs.readFileSync(path.join(credPath, '/msp/keystore/30e9851024149cf1ec183da892c53449e926969d90468cd542879023669c5d4e_sk')).toString();
 
         // Load credentials into wallet
-        const identityLabel = 'Admin@org1.example.com';
-        const identity = X509WalletMixin.createIdentity('Org1MSP', cert, key);
+        const identityLabel = 'Admin@org2.example.com';
+        const identity = X509WalletMixin.createIdentity('Org2MSP', cert, key);
 
         await wallet.import(identityLabel, identity);
 
